6c71915e4ee343a6d7b13e62374eacdd1e88075f
Yuzyzy88 <>
2020-10-19 09:19:39 -0400

    feat: create login, logout, and signup views

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
 
 # Namespace URL paths
 app_name = "tasks"
 
 urlpatterns = [
-    
+    path('', views.index, name="home"),
+    path('signup', views.UserSignUpForm.as_view(), name="signup"),
+    path('login', views.UserLoginView.as_view(), name="login"),
+    path('logout', views.UserLogoutView.as_view(), name="logout"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.views.generic import CreateView
+from .forms import SignUpForm
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    """
+    Renders homepage
+    """
+    return render(request, 'tasks/index.html')
+
+
+def login(request: HttpRequest):
+    """
+    Renders login page
+    """
+    return render(request, 'tasks/login.html')
+
+
+class UserSignUpForm(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('tasks:login')
+    template_name = "tasks/signup.html"
+
+    def get_form_kwargs(self, *args, **kwargs):
+        kwargs = super().get_form_kwargs(*args, **kwargs)
+
+        if self.request.method in ('POST', 'PUT'):
+            username = kwargs['data'].get('username', '')
+            kwargs['data'] = self.request.POST.copy()
+            kwargs['data'].update({'email': username})
+        return kwargs
+
+
+class UserLoginView(LoginView):
+    template_name = "tasks/login.html"
+    extra_context = {'next': reverse_lazy('tasks:home')}
+    redirect_authenticated_user = True
+
+
+class UserLogoutView(LogoutView):
+    template_name = "tasks/logout.html"
+    extra_context = {'logged_out': True}
