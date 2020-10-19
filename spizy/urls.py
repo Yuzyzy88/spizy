7c5fef7970b70b60aa105d555152078a55c6b187
Yuzyzy88 <>
2020-10-19 09:18:30 -0400

    feat: use string urls instead of actual paths

--- spizy/urls.py
+++ spizy/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from tasks.urls import urlpatterns as task_urls
 
 # Root URL paths
 urlpatterns = [
