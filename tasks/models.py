705ac4d27c651b00030a87aab47641d635a57c40
Yuzyzy88 <>
2020-10-21 23:15:48 -0400

    feat: add membership level to access

--- tasks/models.py
+++ tasks/models.py
@@ -3,22 +3,6 @@ from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
 
-class Task(models.Model):
-
-    title = models.TextField()
-    description = models.TextField()
-
-    class Meta:
-        verbose_name = "task"
-        verbose_name_plural = "tasks"
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse("tasks:task_detail", kwargs={"pk": self.pk})
-
-
 class Project(models.Model):
 
     title = models.TextField()
@@ -45,6 +29,12 @@ class ProjectAccess(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class MembershipLevel(models.IntegerChoices):
+        OWNER = 1
+        MEMBER = 2
+
+    membership_level = models.IntegerField(choices=MembershipLevel.choices)
+
     class Meta:
         verbose_name = "projectaccess"
         verbose_name_plural = "projectaccess"
@@ -54,3 +44,21 @@ class ProjectAccess(models.Model):
 
     def get_absolute_url(self):
         return reverse("projectaccess_detail", kwargs={"pk": self.pk})
+
+
+class Task(models.Model):
+
+    title = models.TextField()
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "task"
+        verbose_name_plural = "tasks"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("tasks:task_detail", kwargs={"pk": self.pk})
