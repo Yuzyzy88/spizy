576762c6bd5e217f09a4bdf4badd26660ee494dc
Yuzyzy88 <>
2020-10-25 20:41:02 -0400

    feat: create serializer for ProjectAccess model

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,17 +1,36 @@
-from rest_framework.serializers import ModelSerializer, ReadOnlyField
+from rest_framework.serializers import (EmailField, ModelSerializer,
+                                        ReadOnlyField)
 
-from tasks.models import Project, Task
+from tasks.models import Project, ProjectAccess, Task
 
 
 class ProjectSerializer(ModelSerializer):
+    """
+    Serializer for Project Model
+    """
     class Meta:
         model = Project
         fields = ['id', 'title', 'description']
 
 
 class TaskSerializer(ModelSerializer):
+    """
+    Serializer for Task Model
+    """
+
     owner = ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'project', 'owner']
+
+
+class ProjectAccessSerializer(ModelSerializer):
+    """
+    Serializer for ProjecAccess Model
+    """
+    user = EmailField(source='user.username')
+
+    class Meta:
+        model = ProjectAccess
+        fields = ['id', 'project', 'user', 'membership_level']
