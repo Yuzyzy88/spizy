6f01510b0bfdab8acbcd0ea500baa2aaa967a51e
Yuzyzy88 <>
2020-11-07 20:21:05 -0500

    fix: check if project exists before creating access

--- tasks/views.py
+++ tasks/views.py
@@ -248,8 +248,12 @@ class ProjectAccessList(ListCreateAPIView):
             username=serializer.validated_data.get('user').get('username'))
         # Check if the user is the OWNER of the project;
         # Owners of the project may add access
-        user_access = queryset.filter(user=self.request.user).get(
-            project=project)
+        try:
+            user_access = queryset.filter(user=self.request.user).get(
+                project=project)
+        except ProjectAccess.DoesNotExist:
+            raise PermissionDenied(
+                "Your either don't have access or project doesn't exist")
 
         # Check if the user is the owner
         self.check_object_permissions(self.request, user_access)
